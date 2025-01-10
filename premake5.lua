ab2a2e4e3403d89ff0c894ae1307faaa9d70dee1
KingoBoiii <kingodk@outlook.dk>
2025-01-10 23:00:13 +0100

    Linking Vulkan libraries

--- premake5.lua
+++ premake5.lua
@@ -13,9 +13,19 @@ workspace "Atom"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+-- Grab Vulkan SDK path
+VULKAN_SDK = os.getenv("VULKAN_SDK")
+
 IncludeDir = {}
 IncludeDir['spdlog'] = "%{wks.location}/Atom/Vendor/spdlog/include";
 IncludeDir['glfw'] = "%{wks.location}/Atom/Vendor/glfw/include";
+IncludeDir['vulkan'] = "%{VULKAN_SDK}/Include";
+
+LibraryDir = {}
+LibraryDir['vulkan'] = "%{VULKAN_SDK}/lib";
+
+Library = {}
+Library['vulkan'] = "%{LibraryDir.vulkan}/vulkan-1.lib";
 
 group "Dependencies"
     include "Atom/Vendor/spdlog"
