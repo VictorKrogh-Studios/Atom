3857952176ca0bec2a70edc973ca7164fcbd0c32
KingoBoiii <kingodk@outlook.dk>
2025-01-12 08:19:00 +0100

    Using Pipeline API in SandboxLayer (bug: Pipeline destructor not called, when deleting?)

--- Sandbox/Source/Layers/SandboxLayer.cpp
+++ Sandbox/Source/Layers/SandboxLayer.cpp
@@ -3,14 +3,18 @@
 void SandboxLayer::OnAttach()
 {
 	m_Shader = Atom::Shader::CreateFromFile("Assets/Shaders/static_vert.spv", "Assets/Shaders/static_frag.spv");
+
+	Atom::PipelineOptions pipelineOptions{};
+	pipelineOptions.ImageFormat = Atom::Enumerations::ImageFormat::B8G8R8A8_UNORM;
+	pipelineOptions.Shader = m_Shader;
+	Atom::Pipeline::Create(pipelineOptions);
 }
 
 void SandboxLayer::OnDetach()
 {
+	delete m_Pipeline;
+	m_Pipeline = nullptr;
+
 	delete m_Shader;
 	m_Shader = nullptr;
-}
-
-void SandboxLayer::OnUpdate(float deltaTime)
-{
 }
