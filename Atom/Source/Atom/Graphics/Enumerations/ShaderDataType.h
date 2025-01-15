f6dfed7c0e226de58b015eb109fb3b2c10320efa
KingoBoiii <kingodk@outlook.dk>
2025-01-15 11:12:07 +0100

    Added ShaderDataType enum

---
+++ Atom/Source/Atom/Graphics/Enumerations/ShaderDataType.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace Atom::Enumerations
+{
+
+	enum class ShaderDataType
+	{
+		None = 0, 
+		Float, 
+		Float2, 
+		Float3, 
+		Float4, 
+		Mat3, 
+		Mat4, 
+		Int, 
+		Int2, 
+		Int3, 
+		Int4, 
+		Bool
+	};
+
+}
