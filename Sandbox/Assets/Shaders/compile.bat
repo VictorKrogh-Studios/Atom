a509fcc7ba3fa5b42997c8fedee580875a6662b1
KingoBoiii <kingodk@outlook.dk>
2025-01-15 14:15:14 +0100

    Added shader that uses a uniform buffer

--- Sandbox/Assets/Shaders/compile.bat
+++ Sandbox/Assets/Shaders/compile.bat
@@ -5,3 +5,6 @@ C:/VulkanSDK/1.3.296.0/Bin/glslc.exe static.frag -o static_frag.spv
 
 C:/VulkanSDK/1.3.296.0/Bin/glslc.exe shader.vert -o shader_vert.spv
 C:/VulkanSDK/1.3.296.0/Bin/glslc.exe shader.frag -o shader_frag.spv
+
+C:/VulkanSDK/1.3.296.0/Bin/glslc.exe shader_ubo.vert -o shader_ubo_vert.spv
+C:/VulkanSDK/1.3.296.0/Bin/glslc.exe shader_ubo.frag -o shader_ubo_frag.spv
