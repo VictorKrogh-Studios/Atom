411d7881edaec621233ec5bfbb7786e29bf51626
KingoBoiii <kingodk@outlook.dk>
2025-01-18 22:19:59 +0100

    Removed unused methods

--- Atom/Source/Atom/Graphics/RenderCommand.h
+++ Atom/Source/Atom/Graphics/RenderCommand.h
@@ -16,23 +16,12 @@ namespace Atom
 	public:
 		virtual ~RenderCommand() = default;
 
-		virtual void ResetCommandBuffer(CommandBuffer* commandBuffer, uint32_t frameIndex) const = 0;
-		virtual void BeginCommandBuffer(CommandBuffer* commandBuffer, uint32_t frameIndex) const = 0;
-		virtual void EndCommandBuffer(CommandBuffer* commandBuffer, uint32_t frameIndex) const = 0;
-		virtual void SubmitCommandBuffer(CommandBuffer* commandBuffer, uint32_t frameIndex, bool wait = false) const = 0;
-
 		virtual void BeginRenderPass(CommandBuffer* commandBuffer, RenderPass* renderPass, uint32_t frameIndex) const = 0;
 		virtual void EndRenderPass(CommandBuffer* commandBuffer, uint32_t frameIndex) const = 0;
 
 		virtual void RenderStaticPipeline(CommandBuffer* commandBuffer, Pipeline* pipeline, uint32_t vertexCount, uint32_t frameIndex) const = 0;
 		virtual void DrawVertices(CommandBuffer* commandBuffer, Pipeline* pipeline, VertexBuffer* vertexBuffer, uint32_t vertexCount, uint32_t frameIndex) const = 0;
 		virtual void DrawIndexed(CommandBuffer* commandBuffer, Pipeline* pipeline, VertexBuffer* vertexBuffer, IndexBuffer* indexBuffer, uint32_t indexCount, uint32_t frameIndex) const = 0;
-
-	private: // Should only be called from Renderer!
-		virtual void BeginFrame(uint32_t frameIndex) const = 0;
-		virtual void EndFrame(uint32_t frameIndex) const = 0;
-
-		friend class Renderer;
 	};
 
 }
--- Atom/Source/Atom/Graphics/Vulkan/VulkanRenderCommand.h
+++ Atom/Source/Atom/Graphics/Vulkan/VulkanRenderCommand.h
@@ -21,29 +21,15 @@ namespace Atom
 		VulkanRenderCommand();
 		virtual ~VulkanRenderCommand() = default;
 
-		virtual void ResetCommandBuffer(CommandBuffer* commandBuffer, uint32_t frameIndex) const override;
-		virtual void BeginCommandBuffer(CommandBuffer* commandBuffer, uint32_t frameIndex) const override;
-		virtual void EndCommandBuffer(CommandBuffer* commandBuffer, uint32_t frameIndex) const override;
-		virtual void SubmitCommandBuffer(CommandBuffer* commandBuffer, uint32_t frameIndex, bool wait = false) const override;
-
 		virtual void BeginRenderPass(CommandBuffer* commandBuffer, RenderPass* renderPass, uint32_t frameIndex) const override;
 		virtual void EndRenderPass(CommandBuffer* commandBuffer, uint32_t frameIndex) const override;
 
 		virtual void RenderStaticPipeline(CommandBuffer* commandBuffer, Pipeline* pipeline, uint32_t vertexCount, uint32_t frameIndex) const override;
 		virtual void DrawVertices(CommandBuffer* commandBuffer, Pipeline* pipeline, VertexBuffer* vertexBuffer, uint32_t vertexCount, uint32_t frameIndex) const override;
 		virtual void DrawIndexed(CommandBuffer* commandBuffer, Pipeline* pipeline, VertexBuffer* vertexBuffer, IndexBuffer* indexBuffer, uint32_t indexCount, uint32_t frameIndex) const override;
-
-	private: // Should only be called from Renderer!
-		virtual void BeginFrame(uint32_t frameIndex) const override;
-		virtual void EndFrame(uint32_t frameIndex) const override;
-
-	private:
-		void CreateDrawCommandBuffers(VkDevice device, uint32_t count);
 	private:
 		Internal::VulkanDevice* m_VulkanDevice;
 		VulkanSwapChain* m_SwapChain;
-
-		std::vector<VkCommandBuffer> m_DrawCommandBuffers;
 
 		friend class VulkanImGuiLayer;
 	};
