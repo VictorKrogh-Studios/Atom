781f9e7f4d1c78a26536b0529781cbfd869277f3
KingoBoiii <kingodk@outlook.dk>
2025-02-02 08:47:28 +0100

    Added support for creating a shader directly from pixel data. Also added TextureCreateInfo struct for later use

--- Atom/Include/Atom/Graphics/Texture.h
+++ Atom/Include/Atom/Graphics/Texture.h
@@ -3,14 +3,20 @@
 namespace Atom
 {
 
+	struct TextureCreateInfo
+	{
+	};
+
 	class Texture
 	{
 	public:
-		static Texture* Create(const std::filesystem::path& filepath);
+		static Texture* Create(const std::filesystem::path& filepath, const TextureCreateInfo& createInfo = TextureCreateInfo());
+		static Texture* Create(uint32_t width, uint32_t height, void* data, const TextureCreateInfo& createInfo = TextureCreateInfo());
 	public:
-		Texture(uint32_t width, uint32_t height, uint32_t channels);
+		Texture(uint32_t width, uint32_t height, uint32_t channels, const TextureCreateInfo& createInfo);
 		virtual ~Texture() = default;
 	protected:
+		TextureCreateInfo m_CreateInfo;
 		uint32_t m_Width;
 		uint32_t m_Height;
 		uint32_t m_Channels;
