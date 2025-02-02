fe5a92521a0547f5eafa0fe12d0e6dff47817f2c
KingoBoiii <kingodk@outlook.dk>
2025-02-02 08:47:42 +0100

    Updated Texture source file

--- Atom/Source/Atom/Graphics/Texture.cpp
+++ Atom/Source/Atom/Graphics/Texture.cpp
@@ -6,13 +6,18 @@
 namespace Atom
 {
 
-	Texture* Texture::Create(const std::filesystem::path& filepath)
+	Texture* Texture::Create(const std::filesystem::path& filepath, const TextureCreateInfo& createInfo)
 	{
-		return new VulkanTexture(filepath);
+		return new VulkanTexture(filepath, createInfo);
 	}
 
-	Texture::Texture(uint32_t width, uint32_t height, uint32_t channels)
-		: m_Width(width), m_Height(height), m_Channels(channels)
+	Texture* Texture::Create(uint32_t width, uint32_t height, void* data, const TextureCreateInfo& createInfo)
+	{
+		return new VulkanTexture(width, height, data, createInfo);
+	}
+
+	Texture::Texture(uint32_t width, uint32_t height, uint32_t channels, const TextureCreateInfo& createInfo)
+		: m_Width(width), m_Height(height), m_Channels(channels), m_CreateInfo(createInfo)
 	{
 	}
 
